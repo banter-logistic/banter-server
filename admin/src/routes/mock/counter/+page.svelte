4cb7eb91a036acb21534e6360a3f3d19283c2275
ariaandika <andikaakun17@gmail.com>
2023-09-05 12:30:38 +0700

    using faker data in list and gateout counter

--- admin/src/routes/mock/counter/+page.svelte
+++ admin/src/routes/mock/counter/+page.svelte
@@ -1,18 +1,13 @@
 <script lang=ts>
-  
-  import { Text, Number, Select } from "lib/cp/static";
-  import Product from "lib/cp/static/Div.svelte";
+  import { arr, randLi, randWo } from "lib/faker";
   import Table from "lib/cp/Table.svelte";
   import { getContext } from "svelte";
   
   getContext<any>('title').set('List Barang')
   
-  const data = [
-    { id: '1', name: 'John', desc: 'lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom ', title: "lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom " },
-    { id: '3', name: 'John', desc: 'lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom ', title: "lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom " },
-    { id: '2', name: 'John', desc: 'lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom ', title: "lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom " },
-    { id: '4', name: 'John', desc: 'lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom ', title: "lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom lorem ipsum dom " },
-  ]
+  const data = arr(40, i => {
+    return { id: i+1, name: randWo(), desc: randLi(20) }
+  })
   
   
 </script>
---
+++ admin/src/routes/mock/counter/gateout/+page.svelte
@@ -0,0 +1,51 @@
+
+
+<script lang=ts>
+  import { Text } from "lib/cp/static";
+  import Table from "lib/cp/Table.svelte";
+  import Dump from "lib/cp/Dump.svelte";
+  import { listBarang } from "lib/faker/dummy";
+  import { getContext } from "svelte";
+  
+  const data = listBarang
+  
+  getContext<any>('title').set('Gateway Out')
+  
+</script>
+
+<section class="bg-white shadow-md p-4 rounded-md my-4">
+  
+  <Table {data}>
+    
+    <svelte:fragment slot="th">
+      Action
+    </svelte:fragment>
+    
+    <input type="checkbox" class="form-checkbox" slot="td">
+    
+  </Table>
+  
+  
+</section>
+
+<section class="flex gap-8">
+  
+  
+  <section class="w-break-4 overflow-auto">
+    
+    <Text label="Driver ID" />
+    
+    <button class="btn primary my-2">Pilih Kota</button>
+    
+  </section>
+  
+  
+  
+  <section class="w-break-4 overflow-auto">
+    <h2 class="my-3">Driver</h2>
+    <Dump data={{driver: 'John Doe',vehicle: 'Truck'}} />
+    
+  </section>
+  
+  
+</section>
